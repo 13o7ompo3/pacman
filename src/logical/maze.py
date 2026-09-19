071325db9296b4fc95ff37f91df3373ba12e1666
Blxee <aminedev57@gmail.com>
2026-09-19 11:52:08 +0100

    Changed: super pacgums now spawn at the corners

--- src/logical/maze.py
+++ src/logical/maze.py
@@ -178,10 +178,10 @@ class LogicalMaze:
         """
         positions: set[tuple[int, int]] = set()
         corners = [
-            (1, 1),
-            (self.width - 2, 1),
-            (1, self.height - 2),
-            (self.width - 2, self.height - 2),
+            (0, 0),
+            (self.width - 1, 0),
+            (0, self.height - 1),
+            (self.width - 1, self.height - 1),
         ]
         for cx, cy in corners:
             if self.grid[cy][cx] != 15:
