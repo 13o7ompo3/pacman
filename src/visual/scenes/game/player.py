8cc48e6c7e2154976acb63c40ccb72b2dd979e0d
Blxee <aminedev57@gmail.com>
2026-09-22 10:03:58 +0100

    Fixed: player collision not working when he doesn't move at the start

--- src/visual/scenes/game/player.py
+++ src/visual/scenes/game/player.py
@@ -239,6 +239,8 @@ class Player(Node):
 
         if not self.is_collided:
             self.is_collided = self.get_collided_ghost()
+            if self.is_collided and not self.direction:
+                self.maze.tick_player(Direction.UP, self.is_collided)
         if self.local_position == self.target_position:
             self._step_target_position()
         elif self.direction is not None:
